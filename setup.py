aca41a002c841ee37f0696bffc191a8b628756cc
dat.nguyen <dat.nguyen@icomm.vn>
2020-06-16 21:26:42 +0700

    update version

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="awesome_quiver",
     packages=setuptools.find_packages(),
-    version="0.1.0",
+    version="0.1.1",
     author="nghoangdat",
     author_email="18.hoang.dat.12@gmail.com",
     description="quiver",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NgHoangDat/awesome_quiver.git",
-    download_url="https://github.com/NgHoangDat/awesome_quiver/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/NgHoangDat/awesome_quiver/archive/v0.1.1.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
