a7a53d6e2b44edc23d82ca66c4489a414010814e
datnh21 <v.datnh21@vinai.io>
2021-07-10 11:02:13 +0700

    fix bug

--- lescode/export/exporter.py
+++ lescode/export/exporter.py
@@ -2,6 +2,7 @@ from inspect import currentframe, getmembers, getouterframes, isclass
 from importlib import import_module
 from pathlib import Path
 from typing import Callable, Dict, Any
+from functools import lru_cache
 
 __all__ = [
     'export',
@@ -9,30 +10,45 @@ __all__ = [
 ]
 
 
-def export(predicate:Callable[[Any], bool], idx:int=1, module:Any=None) -> Dict[str, Any]:
+@lru_cache(maxsize=1)
+def _get_cache():
+    return set()
+
+
+def export(predicate:Callable[[Any], bool], idx:int=1, module:Any=None, package:str=None, registry:Dict[str, Any]=None):
     curr_frame = currentframe()
     outer_frames = getouterframes(curr_frame)
 
     call_frame = outer_frames[idx]
-    orig_frame = outer_frames[-1]
-
-    call_dir = Path(orig_frame.frame.f_globals['__file__']).resolve().parent
 
     _globals = call_frame.frame.f_globals
     _locals = call_frame.frame.f_locals
 
     current_file = Path(_globals['__file__']).resolve()
+
+    cached = _get_cache()
+    if (current_file.as_posix(), call_frame.frame.f_lineno) in cached:
+        return
+
+    cached.add((current_file.as_posix(), call_frame.frame.f_lineno)) 
     current_dir = current_file.parent
-    package = current_dir.relative_to(call_dir).as_posix().replace('/', '.')
+
+    if package is None:
+        package = __name__
+        for frame in reversed(outer_frames):
+            call_dir = Path(frame.frame.f_globals['__file__']).resolve().parent
+            if current_dir.as_posix().startswith(call_dir.as_posix()):
+                package = current_dir.relative_to(call_dir).as_posix().replace('/', '.')
+                break
 
     if '__all__' not in _globals:
         _globals['__all__'] = []
 
     __all = _globals['__all__']
 
-    items = {}
     for fn in current_dir.rglob("*.py"):
         path = fn.relative_to(current_dir).as_posix()[:-3]
+        # print(path, package)
         if module is None:
             target = import_module('.' + path.replace('/', '.'), package=package)
         else:
@@ -47,12 +63,11 @@ def export(predicate:Callable[[Any], bool], idx:int=1, module:Any=None) -> Dict[
             if name not in _locals:
                 _locals[name] = cls
                 __all.append(name)
+                
+                if registry is not None:
+                    registry[name] = cls
 
-                items[name] = cls
 
-    return items
-
-
-def export_subclass(*classes, module:Any=None) -> Dict[str, Any]:
+def export_subclass(*classes, module:Any=None, package:str=None, registry:Dict[str, Any]=None):
     predicate = lambda cls: isclass(cls) and issubclass(cls, classes)
-    return export(predicate, idx=2, module=module)
+    export(predicate, idx=2, module=module, package=package, registry=registry)
