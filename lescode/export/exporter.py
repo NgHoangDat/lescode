a76b255d271af8ad531efbcc3de0c1f1a26218ce
datnh21 <v.datnh21@vinai.io>
2021-07-16 09:21:25 +0700

    fix cache bug

--- lescode/export/exporter.py
+++ lescode/export/exporter.py
@@ -1,4 +1,4 @@
-from inspect import currentframe, getmembers, getouterframes, isclass
+from inspect import currentframe, getmembers, getmodule, getouterframes, isclass
 from importlib import import_module
 from pathlib import Path
 from typing import Callable, Dict, Any
@@ -13,25 +13,26 @@ __all__ = [
 
 @lru_cache(maxsize=1)
 def _get_cache():
-    return set()
+    return {}
 
 
 def export(predicate:Callable[[Any], bool], idx:int=1, module:Any=None, package:str=None, registry:Dict[str, Any]=None):
     curr_frame = currentframe()
     outer_frames = getouterframes(curr_frame)
-
     call_frame = outer_frames[idx]
 
     _globals = call_frame.frame.f_globals
-    _locals = call_frame.frame.f_locals
+    if '__all__' not in _globals:
+        _globals['__all__'] = []
+
+    __all = _globals['__all__']
+
+    if call_frame.function == "<module>":
+        _locals = call_frame.frame.f_locals
+    else:
+        _locals = vars(getmodule(call_frame.frame))
 
     current_file = Path(_globals['__file__']).resolve()
-
-    cached = _get_cache()
-    if (current_file.as_posix(), call_frame.frame.f_lineno) in cached:
-        return
-
-    cached.add((current_file.as_posix(), call_frame.frame.f_lineno)) 
     current_dir = current_file.parent
 
     if package is None:
@@ -44,27 +45,34 @@ def export(predicate:Callable[[Any], bool], idx:int=1, module:Any=None, package:
                 package = f"{package}.{subpackage}" if package else subpackage
                 break
 
-    if '__all__' not in _globals:
-        _globals['__all__'] = []
+    cached = _get_cache()
+    unique_key = (current_file.as_posix(), call_frame.frame.f_lineno)
 
-    __all = _globals['__all__']
+    if unique_key in cached:
+        members = cached[unique_key]
+    else:
+        members = []
+        for fn in current_dir.rglob("*.py"):
+            path = fn.relative_to(current_dir).as_posix()[:-3]
+            if path.startswith("__init__"):
+                continue
 
-    for fn in current_dir.rglob("*.py"):
-        path = fn.relative_to(current_dir).as_posix()[:-3]
-        target = module or import_module('.' + path.replace('/', '.'), package=package)
+            target = module or import_module('.' + path.replace('/', '.'), package=package)
 
-        members = getmembers(
-            target, 
-            predicate=predicate
-        )
+            members.extend(getmembers(
+                target, 
+                predicate=predicate
+            ))
 
-        for name, cls in members:
-            if name not in _locals:
-                _locals[name] = cls
-                __all.append(name)
-                
-                if registry is not None:
-                    registry[name] = cls
+        cached[unique_key] = members
+    
+    for name, cls in members:
+        if name not in _locals or name not in __all:
+            _locals[name] = cls
+            __all.append(name)
+
+            if registry is not None:
+                registry[name] = cls
 
 
 def export_subclass(*classes, module:Any=None, package:str=None, registry:Dict[str, Any]=None):
